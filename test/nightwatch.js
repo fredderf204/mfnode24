2be3880c8f19dd4d587ea46aac863d1962f169e2
Michael Friedrich <mifrie@microsoft.com>
2017-01-30 17:52:12 +1100

    changed test

--- test/nightwatch.js
+++ test/nightwatch.js
@@ -1,10 +1,9 @@
 module.exports = {
   'Demo test' : function (browser) {
-    browser
+    client
       .url('http://rBuxmfnode22feat100.azurewebsites.net')
-      .waitForElementVisible('body', 1000)
+      .waitForElementVisible('body', 1000);
       //.waitForElementVisible('table', 1000)
-      .end();
   },
   
     afterEach: function(client, done) {
