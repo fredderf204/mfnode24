d87aaf2a0421d21adaea1ccc80a9273036e2b0b9
Michael Friedrich <mifrie@microsoft.com>
2017-01-30 18:06:20 +1100

    remove custom command

--- test/nightwatch.js
+++ test/nightwatch.js
@@ -5,9 +5,5 @@ module.exports = {
       .waitForElementVisible('body', 1000);
       //.waitForElementVisible('table', 1000)
   },
-  
-    afterEach: function(client, done) {
-        client.customSauceEnd(done);
-    }
 
 };
